3984fa4f9e9c576d8675f4ba1ae97d104809b7d5
achdiathadit <achdiat.dev@gmail.com>
2021-12-06 15:14:31 +0700

    Create ShippingScreen and saveShippingAddress

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -55,7 +55,7 @@ const LoginScreen = ({ location, history }) => {
 					></Form.Control>
 				</Form.Group>
 
-				<Button type='submit' variant='primary'>
+				<Button type='submit' variant='primary' className='mt-4'>
 					Sign In
 				</Button>
 			</Form>
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -83,7 +83,7 @@ const RegisterScreen = ({ location, history }) => {
 					></Form.Control>
 				</Form.Group>
 
-				<Button type='submit' variant='primary'>
+				<Button type='submit' variant='primary' className='mt-4'>
 					Register
 				</Button>
 			</Form>
