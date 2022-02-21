346bea156845ca55328b2a12dbfe166666162532
achdiathadit <achdiat.dev@gmail.com>
2022-02-21 19:24:18 +0700

    Update controller and routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,4 +36,29 @@ const addOrderItems = asyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems };
+// @desc Get order by ID
+// @routes GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+	const order = await Order.findById(req.params.id).populate(
+		'user',
+		'name email	'
+	);
+
+	// if (order) {
+	// 	res.json(order);
+	// } else {
+	// 	res.status(404);
+	// 	throw new Error('Order not found');
+	// }
+
+	// Alternative --> Check if the request was from an admin or if the order user ID was equal to the request user ID
+	if (order && (req.user.isAdmin || order.user._id.equals(req.user._id))) {
+		res.json(order);
+	} else {
+		res.status(404);
+		throw new Error('Order not found');
+	}
+});
+
+export { addOrderItems, getOrderById };
